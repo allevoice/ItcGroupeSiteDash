b06f6f41d85e035019a8ee48dbcfcd1af15a4e3b
CL <rj027@yahoo.fr>
2021-09-17 10:23:09 -0700

    Ajout un nouveau route

--- Site_ITC/appdata/routes/web.php
+++ Site_ITC/appdata/routes/web.php
@@ -14,3 +14,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home.home');
 });
+
+
+Route::get('/contact', function () {
+    return view('home.home');
+});
